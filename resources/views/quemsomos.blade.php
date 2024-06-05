996cd37d1959385c4a2e659b34d5303f40d7fe88
LucasLSM22 <Lucas.marcal@etec.sp.gov.br>
2024-06-04 22:47:34 -0300

    first commit

---
+++ resources/views/quemsomos.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jão</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-body-secondary">
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-1">
+        &nbsp;
+      </div>
+      <div class="col-xxl-10 bg-white" id="div-principal">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="inicio">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="quemsomos">Quem Somos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contato">Contato</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div id="carouselExampleCaptions" class="carousel slide">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="img/img1.jpg" class="d-block w-100" alt="Bootstrap" width="" height="">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Seus Albuns!</h5>
+                <p></p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="img/img2.jpg" class="d-block w-100" alt="Bootstrap" width="" height="">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Seus Albuns!</h5>
+                <p></p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="img/img3.jpg" class="d-block w-100" alt="Bootstrap" width="" height="">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Seus Albuns!</h5>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="container mt-5">
+    <p>
+      <strong>Nome Completo:</strong> João Vitor Romania Balbino
+    </p>
+    <p>
+      <strong>Data de Nascimento:</strong> 3 de novembro de 1994
+    </p>
+    <p>
+      <strong>Origem:</strong> Américo Brasiliense, São Paulo, Brasil
+    </p>
+    <p>
+      Jão é um cantor e compositor brasileiro que se destacou no cenário musical pela sua voz única e letras emocionantes. Ele começou sua carreira lançando covers na internet, o que rapidamente chamou a atenção de um público crescente. Em 2018, lançou seu primeiro álbum, <em>Lobos</em>, que incluía sucessos como "Imaturo" e "Vou Morrer Sozinho".
+    </p>
+    <p>
+      Em 2019, Jão lançou seu segundo álbum, <em>Anti-Herói</em>, consolidando seu estilo e ganhando ainda mais fãs. Com faixas que exploram as complexidades das relações e da vida, ele se tornou uma figura influente na música brasileira.
+    </p>
+    <p>
+      O álbum <em>Pirata</em> (2021) marcou uma nova fase em sua carreira, trazendo um som mais ousado e letras introspectivas. Este álbum contou com hits como "Não Te Amo" e "Idiota", que conquistaram as paradas musicais.
+    </p>
+    <p>
+      Seu trabalho mais recente, <em>Super</em> (2023), celebra sua jornada artística e pessoal, apresentando uma mistura de sons e temas que refletem sua maturidade como artista. Jão continua a inovar e a tocar os corações de muitos com sua música sincera e envolvente.
+    </p>
+  </div>
+  <footer class="bg-body-tertiary py-3">
+    <div class="container">
+      <p class="mb-0">© 2024 Jão. Todos os direitos reservados.</p>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('inicio');
+});
+Route::get('/quemsomos', function () {
+    return view('quemsomos');
+});
+Route::get('/contato', [ContactController::class, 'showForm']);
+Route::post('/contato', [ContactController::class, 'submitForm']);
